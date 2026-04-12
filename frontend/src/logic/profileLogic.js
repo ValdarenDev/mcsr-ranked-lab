a39a371c8d606b8a829b3bc77d958dc82d3e6b29
ValdarenDev <bdog0804@gmail.com>
2026-04-12 01:19:59 -0500

    Add profile error handling

--- frontend/src/logic/profileLogic.js
+++ frontend/src/logic/profileLogic.js
@@ -1,22 +1,25 @@
 import axios from "axios";
 
 export async function getProfile(username) {
-    const res = await axios.get(`https://mcsrranked.com/api/users/${username}`);
-    let userData = res.data.data;
-    let userStats = userData.statistics.season;
-    let userCompletions = userStats.completions.ranked;
-    let userWins = userStats.wins.ranked;
-    let userLosses = userStats.loses.ranked;
+    try {
+        const res = await axios.get(`https://mcsrranked.com/api/users/${username}`);
+        let userData = res.data.data;
+        let userStats = userData.statistics.season;
+        let userCompletions = userStats.completions.ranked;
+        let userWins = userStats.wins.ranked;
+        let userLosses = userStats.loses.ranked;
 
-    let userRank = userData.eloRank;
-    let userElo = userData.eloRate;
-    let userAverage = timeConversion(userStats.completionTime.ranked/userCompletions);
-    let userWinstreak = userStats.currentWinStreak.ranked;
-    let userWinLoss = `${userWins} - ${userLosses}`;
-    let userPb = timeConversion(userStats.bestTime.ranked);
+        let userRank = userData.eloRank;
+        let userElo = userData.eloRate;
+        let userAverage = timeConversion(userStats.completionTime.ranked/userCompletions);
+        let userWinstreak = userStats.currentWinStreak.ranked;
+        let userWinLoss = `${userWins} - ${userLosses}`;
+        let userPb = timeConversion(userStats.bestTime.ranked);
 
-    return [userRank, userElo, userAverage, userWinstreak, userWinLoss, userPb];
-
+        return [userRank, userElo, userAverage, userWinstreak, userWinLoss, userPb];
+    } catch {
+        return ["This user has not played ranked before, please try a different IGN!"];
+    }
 }
 
 function timeConversion(time) {
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getProfile } from "../logic/profileLogic";
 import Chart from "chart.js/auto";
 import "../css/global.css";
@@ -8,17 +8,24 @@ export default function ProfilePage() {
   const { ign } = useParams();
   console.log("IGN from URL:", ign);
 
+  const navigate = useNavigate();
+  const [searchIgn, setSearchIgn] = useState(ign);
   const [runs, setRuns] = useState([]);
   const [sortMode, setSortMode] = useState({ column: "date", direction: "desc" });
   const [pageSize, setPageSize] = useState(10);
   const [page, setPage] = useState(1);
   const [profileInfo, setProfileInfo] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await getProfile(ign);
+      if (result.length === 1) {
+        setError(result[0]);
+        return;
+      }
       setProfileInfo(result);
-    }
+    };
     fetchData();
   }, [ign]);
 
@@ -26,7 +33,6 @@ export default function ProfilePage() {
   const winLossRef = useRef(null);
   const completionRef = useRef(null);
 
-  // Sample data (replace later with API)
   const sampleProfile = {
     ign: ign || "Unknown Player",
     rank: profileInfo[0],
@@ -169,112 +175,139 @@ export default function ProfilePage() {
     });
   }
 
-  return (
-    <main className="profile-layout">
-      <h2>{sampleProfile.ign}</h2>
+  if (error) {
+    return (
+      <main className="profile-layout">
+        <section className="mc-panel error-panel">
+          <h2>Error Loading Profile...</h2>
+          <p>{error}</p>
+          <input
+            className="mc-input"
+            type="text"
+            placeholder="Search IGN"
+            value={searchIgn}
+            onChange={(e) => setSearchIgn(e.target.value)}
 
-      <section className="profile-nav">
-        <select onChange={(e) => {
-          const sectionId = e.target.value;
-          if (sectionId) {
-            document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
-          }
-        }}>
-          <option value="">Jump to section...</option>
-          <option value="rank-section">Rank</option>
-          <option value="elo-section">Elo</option>
-          <option value="avg-section">Average Completion Time</option>
-          <option value ="streak-section">Win Streak</option>
-          <option value="wl-section">Win-Loss Record</option>
-          <option value="pb-section">Personal Best</option>
-          <option value="trends-section">Recent Trends</option>
-          <option value="runs-section">Runs</option>
-        </select>
-      </section>
+          ></input>
 
-      <section id="rank-section" className="mc-panel">
-        <h3>Rank</h3>
-        <p>{sampleProfile.rank}</p>
-      </section>
+          <button className="mc-button" onClick={() => navigate(`/profile/${searchIgn}`)}>
+            Search
+          </button>
+        </section>
+      </main>
+    );
+  }
 
-      <section id="elo-section" className="mc-panel">
-        <h3>Elo</h3>
-        <p>{sampleProfile.elo}</p>
-      </section>
+  if (profileInfo.length === 0) {
+    return <p>Loading...</p>;
+  } else {
+      return (
+        <main className="profile-layout">
+          <h2>{sampleProfile.ign}</h2>
 
-      <section id="avg-section" className="mc-panel">
-        <h3>Average Completion Time</h3>
-        <p>{sampleProfile.averageCompletionTime}</p>
-      </section>
+          <section className="profile-nav">
+            <select onChange={(e) => {
+              const sectionId = e.target.value;
+              if (sectionId) {
+                document.getElementById(sectionId)?.scrollIntoView({ behavior: "smooth" });
+              }
+            }}>
+              <option value="">Jump to section...</option>
+              <option value="rank-section">Rank</option>
+              <option value="elo-section">Elo</option>
+              <option value="avg-section">Average Completion Time</option>
+              <option value ="streak-section">Win Streak</option>
+              <option value="wl-section">Win-Loss Record</option>
+              <option value="pb-section">Personal Best</option>
+              <option value="trends-section">Recent Trends</option>
+              <option value="runs-section">Runs</option>
+            </select>
+          </section>
 
-      <section id="streak-section" className="mc-panel">
-        <h3>Win Streak</h3>
-        <p>{sampleProfile.winStreak}</p>
-      </section>
+          <section id="rank-section" className="mc-panel">
+            <h3>Rank</h3>
+            <p>{sampleProfile.rank}</p>
+          </section>
 
-      <section id="wl-section" className="mc-panel">
-        <h3>Win-Loss Record</h3>
-        <p>{sampleProfile.wlRecord}</p>
-      </section>
+          <section id="elo-section" className="mc-panel">
+            <h3>Elo</h3>
+            <p>{sampleProfile.elo}</p>
+          </section>
 
-      <section id="pb-section" className="mc-panel">
-        <h3>Personal Best</h3>
-        <p>{sampleProfile.personalBest}</p>
-      </section>
+          <section id="avg-section" className="mc-panel">
+            <h3>Average Completion Time</h3>
+            <p>{sampleProfile.averageCompletionTime}</p>
+          </section>
 
-      <section id="trends-section" className="mc-panel">
-        <h3>Recent Trends</h3>
-        <div className="charts-row">
-          <canvas ref={winLossRef}></canvas>
-          <canvas ref={completionRef}></canvas>
-        </div>
-      </section>
+          <section id="streak-section" className="mc-panel">
+            <h3>Win Streak</h3>
+            <p>{sampleProfile.winStreak}</p>
+          </section>
 
-      <section id="runs-section" className="mc-panel">
-        <h3>Runs</h3>
+          <section id="wl-section" className="mc-panel">
+            <h3>Win-Loss Record</h3>
+            <p>{sampleProfile.wlRecord}</p>
+          </section>
 
-        <div className="run-page-size">
-          <label className="entries-label">Show</label>
+          <section id="pb-section" className="mc-panel">
+            <h3>Personal Best</h3>
+            <p>{sampleProfile.personalBest}</p>
+          </section>
 
-          <select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
-            <option value={10}>10</option>
-            <option value={25}>25</option>
-            <option value={30}>30</option>
-            <option value={50}>50</option>
-          </select>
+          <section id="trends-section" className="mc-panel">
+            <h3>Recent Trends</h3>
+            <div className="charts-row">
+              <canvas ref={winLossRef}></canvas>
+              <canvas ref={completionRef}></canvas>
+            </div>
+          </section>
 
-          <label className="entries-label">entries</label>
-        </div>
+          <section id="runs-section" className="mc-panel">
+            <h3>Runs</h3>
 
-        <p>Total Runs: {runs.length}</p>
+            <div className="run-page-size">
+              <label className="entries-label">Show</label>
 
-        <table className="run-table">
-          <thead>
-            <tr>
-              <th onClick={() => handleSort("result")}>Result</th>
-              <th onClick={() => handleSort("time")}>Time</th>
-              <th onClick={() => handleSort("opponent")}>Opponent</th>
-              <th onClick={() => handleSort("date")}>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {paginatedRuns.map((run, i) => (
-              <tr key={i}>
-                <td>{run.result}</td>
-                <td>{run.time}</td>
-                <td>{run.opponent}</td>
-                <td>{run.date}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        
-        <div className="pagination">
-            <button disabled={page === 1} onClick={() => setPage(page - 1)}>Prev</button>
-            <span>Page {page}/{totalPages}</span>
-            <button disabled={page === totalPages} onClick={() => setPage(page + 1)}>Next</button>
-        </div>
-      </section>
-    </main>
-  );
+              <select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
+                <option value={10}>10</option>
+                <option value={25}>25</option>
+                <option value={30}>30</option>
+                <option value={50}>50</option>
+              </select>
+
+              <label className="entries-label">entries</label>
+            </div>
+
+            <p>Total Runs: {runs.length}</p>
+
+            <table className="run-table">
+              <thead>
+                <tr>
+                  <th onClick={() => handleSort("result")}>Result</th>
+                  <th onClick={() => handleSort("time")}>Time</th>
+                  <th onClick={() => handleSort("opponent")}>Opponent</th>
+                  <th onClick={() => handleSort("date")}>Date</th>
+                </tr>
+              </thead>
+              <tbody>
+                {paginatedRuns.map((run, i) => (
+                  <tr key={i}>
+                    <td>{run.result}</td>
+                    <td>{run.time}</td>
+                    <td>{run.opponent}</td>
+                    <td>{run.date}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            
+            <div className="pagination">
+                <button disabled={page === 1} onClick={() => setPage(page - 1)}>Prev</button>
+                <span>Page {page}/{totalPages}</span>
+                <button disabled={page === totalPages} onClick={() => setPage(page + 1)}>Next</button>
+            </div>
+          </section>
+        </main>
+      );
+  }
 }
